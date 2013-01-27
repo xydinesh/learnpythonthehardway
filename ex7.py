6ae750026e047d1f631be4045171732e8756548f
Dinesh Weerapurage <xydinesh@gmail.com>
2013-01-26 23:12:09 -0500

    add exercise 7

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# printing string
+print "Mary had a little lamb."
+# printing string and pass 'snow' as string option
+print "its fleece was white as %s." % "snow"
+# printing string
+print "and everywhere that Mary went."
+# print 10 '.'
+print "." * 10  # what'd that do
+
+# Assign characters of 'Cheese Burger' into variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try romving at to what happens
+
+# print all characters, comma is important "not" to include a newline
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
