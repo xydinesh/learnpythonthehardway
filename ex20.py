4964541d07f8b9a054b21f56c14c701177e351a1
Dinesh Weerapurage <xydinesh@gmail.com>
2013-02-15 03:23:26 +0000

    adding drills for ex20

--- ex20.py
+++ ex20.py
@@ -1,34 +1,42 @@
 from sys import argv
 script, input_file = argv
 
+# Read content of a file
 def print_all(f):
     print f.read()
 
+# Reset the file pointer of file, Oh, I forgot this is python not C
 def rewind(f):
     f.seek(0)
 
+# Print a line
 def print_a_line(line_count, f):
     print line_count, f.readline()
 
+# Open current file named "input_file" to read
 current_file = open(input_file)
 
 print "first let's print the whole file:\n"
 
+# Print everything form the input file
 print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
 
+# Rewind the file
 rewind(current_file)
 
 print "Let's print three liness:"
 
+# Printing line by line
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+# Change source code to use += notation
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
              
                     
